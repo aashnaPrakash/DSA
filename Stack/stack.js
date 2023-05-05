43c4c0c27846a6eeb498e3a50531284b5fa3161e
aashna-prakash <aashna.prakash@sap.com>
2023-05-05 20:36:48 +0530

    stack and queue

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,13 +1,12 @@
 export class Stack {
     constructor(){
         this.items = [];
-        this.minimum = null;
+       
     }
 
     push(item){
         this.items.push(item);
-        if(this.items.length === 1) this.minimum = item;
-        else this.updateMin(item);
+      
     }
     pop(){
         if(!this.isEmpty()) return this.items.pop();
@@ -29,16 +28,6 @@ export class Stack {
         console.log(str);
     }
 
-    updateMin(item){
-        if(item < this.minimum){
-            this.minimum = item;
-        }
-    }
-
-    getMin(){
-        return this.minimum;
-    }
-
 }
 
 const main = () => {
@@ -49,7 +38,7 @@ const main = () => {
     console.log(st.getMin());
     st.push(2);
     st.push(15);
-    console.log(st.getMin());
+    
 }
 
 main();
